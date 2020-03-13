319e2e89cd80f82bf78dc71c9f70ea5fee2eab18
xyan <xyan@murex.com>
2020-03-13 18:36:02 +0100

    添加形近字

--- src/main/java/com/xubo/data/ChineseData.java
+++ src/main/java/com/xubo/data/ChineseData.java
@@ -4,14 +4,9 @@ import com.xubo.data.book.Book;
 import com.xubo.data.book.BookSource;
 import com.xubo.data.book.common.CommonBookSource;
 import com.xubo.data.book.renjiao.RenJiaoBookSource;
-import com.xubo.data.character.Character;
-import com.xubo.data.dictionary.Dictionary;
-import com.xubo.data.dictionary.DictionaryEntry;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ChineseData {
@@ -32,7 +27,8 @@ public class ChineseData {
     private List<BookSource> getBookSources() {
         return Arrays.asList(
                 new RenJiaoBookSource(), 
-                new CommonBookSource("/book/character_commonly_used.txt")
+                new CommonBookSource("/book/character_commonly_used.txt"),
+                new CommonBookSource("/book/character_similar.txt")
         );
     }
 
